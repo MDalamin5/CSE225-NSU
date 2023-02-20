2bec98d76cf30e9325b3f1688d8db191c7d46c76
MDalamin5 <mdal.amin5@northsouth.edu>
2023-02-20 13:19:07 +0600

    lab 0

---
+++ Lab Class/Lab-0/lab00/lab00.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    int prime, count = 0;
+
+    cout << "Please enter a number to check whether it's a prime or not: ";
+    cout << endl;
+
+    cin >> prime;
+
+    for(int i=2; i<=prime; i++)
+    {
+        if(prime % i == 0)
+            count++;
+    }
+
+    if(count == 1)
+        cout << prime << " is a prime number!" << endl;
+    else
+        cout << prime << " is not a prime number!" << endl;
+
+    */
+
+
+
+}
---
+++ Lab Class/Lab-0/lab00/lab00/box.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Box{
+
+private:
+    int length, width, height;
+
+public:
+    Box(int, int, int);
+    int getVolume();
+
+
+};
