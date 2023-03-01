d31744541240e3e609ae605e964bb78748cdb34d
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-01 13:37:23 +0600

    #ifdef and not def

---
+++ Lab Class/Lab-1/ifdef ifndef/main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Alamin 14
+#define hello_mode
+int main()
+{
+    #ifdef hello_mode
+    cout<<"Hello"<<endl;
+    #endif
+    cout<<Alamin;
+}
