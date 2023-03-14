4434b53752ee471e4cf3cb14674e2d1d8c822688
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-14 12:27:26 +0600

    student info project

---
+++ Project/Theory/Student management/main.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    string name;
+    int id;
+    double cgpa;
+    string dep;
+    string major_programe;
+    node *next;
+};
+class studentMangement
+{
+    public:
+    node *head;
+    int size_of_student;
+    node *tail;
+
+    studentMangement()
+    {
+        head=NULL;
+        tail=NULL;
+        size_of_student=0;
+    }
+
+    void addStudent()
+    {
+
+    }
+    void displayStudent()
+    {
+
+    }
+    void modefyStudent()
+    {
+
+    }
+    void searchStudent()
+    {
+        
+    }
+    void eraseStudent()
+    {
+
+    }
+};
+int main()
+{
+    cout<<"Project is Cooking......";
+    return 0;
+}
