4bd0f7a3778af8b842db670fa06bdd455fc12ec4
MDalamin5 <mdal.amin5@northsouth.edu>
2023-05-07 20:03:31 +0600

    lab - 7

---
+++ Lab Class/Lab-7.1/main.cpp
@@ -0,0 +1,10 @@
+#include "sortedtype.cpp"
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    SortedType<int> sll;
+    sll.InsertItem(2);
+    sll.InsertItem(3);
+    sll.InsertItem(1);
+}
