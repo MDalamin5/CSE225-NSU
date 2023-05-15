d30a2f5c34244bb4884788c933666b52152e9c32
MDalamin5 <mdal.amin5@northsouth.edu>
2023-05-16 00:10:32 +0600

    lab-3

---
+++ Lab Class/Mid (1-9)/Lab-03/Complex Arr/complex.cpp
@@ -0,0 +1,25 @@
+#include "complex.h"
+#include <iostream>
+using namespace std;
+Complex::Complex()
+{
+    Real = 0;
+    Imaginary = 0;
+}
+Complex::Complex(double r, double i)
+{
+    Real = r;
+    Imaginary = i;
+}
+Complex Complex::operator+(Complex a)
+{
+    Complex t;
+    t.Real = Real + a.Real;
+    t.Imaginary = Imaginary + a.Imaginary;
+    return t;
+}
+void Complex::Print()
+{
+    cout << Real << endl;
+    cout << Imaginary << endl;
+}
