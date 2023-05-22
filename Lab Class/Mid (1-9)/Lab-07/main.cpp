cdca7aefcae03db94a6cb65c3d39907411649d2a
MDalamin5 <mdal.amin5@northsouth.edu>
2023-05-22 12:24:47 +0600

    lab-mid

---
+++ Lab Class/Mid (1-9)/Lab-07/main.cpp
@@ -0,0 +1,10 @@
+#include "sortedtype.cpp"
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    SortedType<int> ob;
+    ob.InsertItem(10);
+    ob.InsertItem(15);
+   // ob.InsertItem(11);
+}
