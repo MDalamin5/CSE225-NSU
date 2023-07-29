1393071f202b862d9bded630d9842295abe7945d
MDalamin5 <mdal.amin5@northsouth.edu>
2023-07-29 13:33:41 +0600

    string addition

--- Lab Class/Jamala/stringadd.cpp
+++ Lab Class/Jamala/stringadd.cpp
@@ -1,55 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     string input1, input2;
-    getline(cin,input1);
+    getline(cin, input1);
     getline(cin, input2);
-     queue<char> q1;
-    queue<char>q2;
-    queue<char>result;
-    cout<<input1.size()<<endl;
-    cout<<input2.length()<<endl;
-    for(int i =0; i<input1.length();i++)
+    queue<char> q1;
+    queue<char> q2;
+    queue<char> result;
+    
+    for (int i = 0; i < input1.length(); i++)
     {
         q1.push(input1[i]);
-        //cout<<input1[i]<<" ";
     }
-    for(int i =0; i<input2.length();i++)
+    for (int i = 0; i < input2.length(); i++)
     {
         q2.push(input2[i]);
-        //cout<<input2[i]<<" ";
     }
-    cout<<endl<<endl;
- 
-//  int q1_size = q1.size();
-//  for(int i=0;i<q1_size;i++)
-//    {
-//     cout<<q1.front()<<" ";
-//     q1.pop();
-//    }
 
-//    int q2_size = q2.size();
-//    for(int i=0;i<q2_size;i++)
-//    {
-//     cout<<q2.front()<<" ";
-//     q2.pop();
-//    }
+    int min_size = min(q1.size(), q2.size());
+    int max_size = max(q1.size(), q2.size());
+    for (int i = 0; i < min_size; i++)
+    {
+        result.push(q1.front());
+        q1.pop();
+        result.push(q2.front());
+        q2.pop();
+    }
 
-   for(int i=0;i<5;i++)
-   {
-    result.push(q1.front());
-      q1.pop();
-      result.push(q2.front());
-      q2.pop();
-   }
-   //cout<<"HElo";
-   for(int i=0;i<10;i++)
-   {
-    cout<<result.front()<<" ";
-    result.pop();
-   }
+    int left_size = max_size - min_size;
+    for (int i = 0; i < left_size; i++)
+    {
+        if (q1.size() > q2.size())
+        {
+            result.push(q1.front());
+            q1.pop();
+        }
+        else if (q2.size() > q1.size())
+        {
+            result.push(q2.front());
+            q2.pop();
+        }
+    }
 
-
-   
+    int res_size = result.size();
+    for (int i = 0; i < res_size; i++)
+    {
+        cout << result.front() << " ";
+        result.pop();
+    }
 }
